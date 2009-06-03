ed471e9450b668328d0a72f6535fde426ef426a3
Jarkko Oranen <jarkko.oranen@tut.fi>
2009-06-03 20:47:03 +0300

    Viper tweaks

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -11,6 +11,9 @@
  (inhibit-startup-screen t)
 
  (viper-mode t)
+ (viper-ex-style-editing nil)
+ (viper-ex-style-motion nil)
+ (viper-vi-style-in-minibuffer nil)
  (viper-shift-width 4)
 
  (viper-vi-state-mode-list (quote (fundamental-mode makefile-mode awk-mode m4-mode xrdb-mode winmgr-mode autoconf-mode cvs-edit-mode html-mode html-helper-mode emacs-lisp-mode lisp-mode lisp-interaction-mode jde-mode java-mode cc-mode c-mode c++-mode objc-mode fortran-mode f90-mode basic-mode bat-mode asm-mode prolog-mode flora-mode sql-mode text-mode indented-text-mode tex-mode latex-mode bibtex-mode ps-mode diff-mode idl-mode perl-mode cperl-mode javascript-mode tcl-mode python-mode sh-mode ksh-mode csh-mode gnus-article-mode mh-show-mode clojure-mode slime-repl-mode)))))
