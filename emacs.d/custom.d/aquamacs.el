4a7ecf97f188614feab277fee0768ee3b756864a
Jarkko Oranen <jarkko.oranen@tut.fi>
2009-06-26 22:29:55 +0300

    Reorganise emacs configuration completely

---
+++ emacs.d/custom.d/aquamacs.el
@@ -0,0 +1,20 @@
+
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(aquamacs-additional-fontsets nil t)
+ '(aquamacs-customization-version-id 180 t)
+ '(aquamacs-tool-bar-user-customization nil t)
+ '(clojure-mode-use-backtracking-indent t)
+ '(emulate-mac-finnish-keyboard-mode t)
+ '(ns-tool-bar-display-mode (quote both) t)
+ '(ns-tool-bar-size-mode (quote regular) t)
+ '(transient-mark-mode t))
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
